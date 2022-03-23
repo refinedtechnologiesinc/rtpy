39ea5e559c2722e3a989748d31b5c458593e95dc
prestonpope192 <prestonpope192@gmail.com>
2022-03-22 20:08:24 -0500

    auto_dbt updates

--- sql_ops/model_raw_to_analytics.py
+++ sql_ops/model_raw_to_analytics.py
@@ -1,17 +1,12 @@
 # %% take raw table and assuming no name changes, make the stg tbl and dbt code
-import snowflake.connector
-from sgs.util.auth import _getvar
-import pprint as pp
 import re
 import clipboard
-import logging
 import sgs.db.snowflake as sf
-from sgs.util.convert import _response
-from sgs.util.sql import create_table_sql
+from sgs.db.dbt.craft import craft_dbt_model
 
 # %% inputs
-schema = "iview_projects_stead"
-raw_table = "system"
+schema = "itrack_fivetran"
+raw_table = "itrackcontracts"
 fct_dim = "dim"
 # deived
 stg_table = f"stg_{schema}__{raw_table}"
@@ -20,30 +15,29 @@ core_table = f"{fct_dim}_{raw_table}"
 
 # %% Open connection
 resp_c = sf.connect("LOADING", "RAW", schema)
-if resp_c['status'] == 200:
-    conn_raw = resp_c['data']
+if resp_c["status"] == 200:
+    conn_raw = resp_c["data"]
 
 # %% get the columns from the RAW table
 resp_c = sf.get_columns(conn_raw, raw_table, sort=True)
-if resp_c['status'] != 200:
+if resp_c["status"] != 200:
     raise ValueError(resp_c)
 
-cols = resp_c['data']
+cols = resp_c["data"]
 # add the id columns that dbt will use as the unique key
-cols.append({'Name': "id", "Type": "VARCHAR(40)",
-            "Unique": "Y", "NotNull": "Y"})
+cols.append({"Name": "id", "Type": "VARCHAR(40)", "Unique": "Y", "NotNull": "Y"})
 
 # %% do some auto cleaning IF NEEDED
 for c in cols:
-    v = c['Name']
+    v = c["Name"]
     # set to lower
     v = v.lower()
     # replace space with _
     v = v.replace(" ", "_")
     # remove non-alphanumeric
-    v = re.sub(r'[^a-zA-Z0-9_]', '', v)
+    v = re.sub(r"[^a-zA-Z0-9_]", "", v)
     # update the rename_map
-    c['As'] = v
+    c["As"] = v
 
 # pp.pprint(rename_map)
 # clipboard.copy(str(rename_map))
@@ -90,10 +84,10 @@ staging as (
 
 for c in cols:
     # skip id col since that gets defined as the sha1 above
-    if c.get('Name') == 'id':
+    if c.get("Name") == "id":
         continue
-    stg_dbt += c.get('Name')
-    spaces = 60 - len(c.get('As', c['Name']))
+    stg_dbt += c.get("Name")
+    spaces = 60 - len(c.get("As", c["Name"]))
     stg_dbt += " " * spaces
     stg_dbt += f"""as {c.get('As', c['Name']).lower()},
         """
@@ -111,42 +105,23 @@ clipboard.copy(stg_dbt)
 
 
 # %% create the schema.yml for this model
-yml = f"- name: {stg_table}\n    description:  This table contains a staged version of '____' from ___.\n    columns:\n"
+yml = (
+    f"- name: {stg_table}\n    description:  This table contains a staged version of '____' from ___.\n    columns:\n"
+)
 for c in cols:
     yml += f"      - name: {c.get('Name').lower()}\n        description: _____\n        tests:\n"
-    if c.get('NotNull') == "Y" or c.get('PrimaryKey') == "Y":
+    if c.get("NotNull") == "Y" or c.get("PrimaryKey") == "Y":
         yml += "          - not_null\n"
-    if c.get('UniqueKey') == "Y":
+    if c.get("UniqueKey") == "Y":
         yml += "          - unique\n"
     yml += "   \n"
 clipboard.copy(yml)
 
-# %% PASTE THAT INTO THE dbt repo the make an needed modification
 
-# Open ANALYTICS connection
-# resp_ca = sf.connect(warehouse="TRANSFORMING",
-#                      db="ANALYTICS",
-#                      schema="STAGING",
-#                      role="TRANSFORMER")
-# if resp_ca['status'] == 200:
-#     conn_a = resp_ca['data']
-
-#  get the columns from the STG table
-# resp_c = sf.get_columns(conn_a, stg_table, sort=True)
-# if resp_c['status'] != 200:
-#     raise ValueError(resp_c)
-
-# cols = resp_c['data']
-#   Create the CORE table sql
-# resp_cs = create_table_sql(core_table, cols)
-
-# if resp_cs['status'] != 200:
-#     raise ValueError(resp_cs)
-
-# c_sql = resp_cs['data']
-# clipboard.copy(c_sql)
-
-#  RUN THIS CREATE SQL IN CORE
+#     # Write the file out again
+# with open(f'_{to_tbl}_schema.yml', 'w') as file:
+#     file.write(model_yml)
+# file.close()
 
 # %% =============== make the dbt core model sql
 core_dbt = f"""with staging as (
@@ -183,9 +158,9 @@ for c in cols:
     # if c.get('PrimaryKey') != "Y":
     #     continue
     yml += f"      - name: {c.get('Name').lower()}\n        description: _____\n        tests:\n"
-    if c.get('NotNull') == "Y" or c.get('PrimaryKey') == "Y":
+    if c.get("NotNull") == "Y" or c.get("PrimaryKey") == "Y":
         yml += "          - not_null\n"
-    if c.get('UniqueKey') == "Y":
+    if c.get("UniqueKey") == "Y":
         yml += "          - unique\n"
     yml += "   \n"
 clipboard.copy(yml)
